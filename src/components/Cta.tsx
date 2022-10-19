0ddde63081da73253bd5cf8a768d153fde1c751b
gbertl <gilbertlcsndle@gmail.com>
2022-10-19 10:49:09 +0800

    correct cta text

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -9,7 +9,7 @@ const Cta = () => {
   return (
     <section className="cta section-margin">
       <Container className="d-flex justify-content-between flex-column flex-lg-row align-items-start">
-        <h1 className="mb-4">Join with me for get special discount</h1>
+        <h1 className="mb-4">Join with me to get special discount</h1>
         <PrimaryBlock className="d-inline-flex gap-2 gap-lg-3 align-items-center w-auto">
           Learn More
           <Image src={arrowRight} />
