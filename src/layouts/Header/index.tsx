7e8e57efc5c299c20097b79a334a3f42c6407287
gbertl <gilbertlcsndle@gmail.com>
2022-11-20 21:03:01 +0800

    add more details in basket button

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -2,10 +2,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
+import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Badge from 'react-bootstrap/Badge';
 
-import logo from '../assets/images/icons/logo.svg';
-import bag from '../assets/images/icons/bag.svg';
-import user from '../assets/images/icons/user.svg';
+import logo from '../../assets/images/icons/logo.svg';
+import bag from '../../assets/images/icons/bag.svg';
+import user from '../../assets/images/icons/user.svg';
+
+import './style.scss';
 
 const Header = () => {
   return (
@@ -24,9 +29,17 @@ const Header = () => {
           </Nav>
 
           <Nav className="ms-auto flex-row gap-3">
-            <Nav.Link>
-              <Image src={bag} />
-            </Nav.Link>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip id="tooltip-example">No items in cart</Tooltip>}
+            >
+              <Nav.Link className="position-relative">
+                <Image src={bag} />
+                <Badge className="rounded-circle position-absolute start-100 translate-middle">
+                  0
+                </Badge>
+              </Nav.Link>
+            </OverlayTrigger>
             <Nav.Link>
               <Image src={user} />
             </Nav.Link>
